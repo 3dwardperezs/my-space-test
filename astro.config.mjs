30147d0781ba5703ced0333af79dead8cbdcbb90
3dwardperezs <3dwardperezs@gmail.com>
2024-08-04 18:11:49 +0100

    new changes 5

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,5 +3,5 @@ import { defineConfig } from 'astro/config';
 // https://astro.build/config
 export default defineConfig({
   site: 'https://3dwardperezs.github.io',
-  base: 'my-space-test',
+  base: 'my-space-test'
 });
