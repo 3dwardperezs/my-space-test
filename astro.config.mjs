3d8c0470f9951c80394b4488fe22de63687f0ddc
3dwardperezs <3dwardperezs@gmail.com>
2024-08-04 21:02:46 +0100

    new changes 15

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,12 @@
 import { defineConfig } from 'astro/config';
-import tailwind from "@astrojs/tailwind";
-import svelte from "@astrojs/svelte";
+import tailwind from '@astrojs/tailwind';
+import svelte from '@astrojs/svelte';
 import netlify from '@astrojs/netlify';
-// https://astro.build/config
+
 export default defineConfig({
-  integrations: [tailwind(), svelte()],
-  adapter: netlify(),
-  site: 'https://3dwardperezs.github.io',
-  base: 'my-space-test'
+  integrations: [
+    tailwind(),
+    svelte(),
+    netlify()
+  ],
 });
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,5 @@
-  /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    './src/**/*.{astro,html,js,jsx,ts,tsx,vue,svelte}',
-  ],
-  theme: {
-    extend: {},
-  },
+  content: ['./src/**/*.{astro,html,js,jsx,ts,tsx}'],
+  theme: { extend: {} },
   plugins: [],
 };
-
-  
