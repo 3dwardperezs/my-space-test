0058fd2e336de11615caf77030b538c5fca1ddb4
3dwardperezs <3dwardperezs@gmail.com>
2024-08-04 17:38:26 +0100

    first commit

---
+++ astro.config.mjs
@@ -0,0 +1,7 @@
+import { defineConfig } from 'astro/config';
+
+// https://astro.build/config
+export default defineConfig({
+  site: 'https://3dwardperezs.github.io',
+  base: 'my-space-test',
+});
